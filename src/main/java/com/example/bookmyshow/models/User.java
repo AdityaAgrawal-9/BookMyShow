054674d59ced5edc5aed0260cdee20ffefceb157
AdityaAgrawal-9 <71038184+AdityaAgrawal-9@users.noreply.github.com>
2025-01-10 11:13:56 +0530

    First day commit and push

---
+++ src/main/java/com/example/bookmyshow/models/User.java
@@ -0,0 +1,15 @@
+package com.example.bookmyshow.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    String name;
+    String email;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
